e8895d44df8f4b86934f6ef58e4e8c0bfc6238d8
snailda7 <snailda7@gmail.com>
2016-04-25 12:37:55 -0400

    fixed the missing save

--- ModelGen.py
+++ ModelGen.py
@@ -1,16 +1,13 @@
-'''from __future__ import print_function
+from __future__ import print_function
 
 
 from pyspark import SparkContext
-# $example on$
 from pyspark.mllib.regression import LabeledPoint, LinearRegressionWithSGD, LinearRegressionModel
-# $example off$
 
 if __name__ == "__main__":
 
     sc = SparkContext(appName="Model")
 
-    # $example on$
     # Load and parse the data
     def parsePoint(line):
         values = [float(x) for x in line.replace(',', ' ').split(' ')]
@@ -21,5 +18,5 @@ if __name__ == "__main__":
 
     # Build the model
     model = LinearRegressionWithSGD.train(parsedData, iterations=5, step=0.1)
-
-    sameModel = LinearRegressionModel.load(sc, "BAC")'''
+    #save the model
+    model.save(sc, "../BAC")
